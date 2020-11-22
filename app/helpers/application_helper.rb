17e79abff2cc3cda3c0214eb732c35b725946769
rloterh <rloterh@gmail.com>
2020-11-22 12:07:58 +0000

    Update views and helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def nav_buttons
+    if logged_in?
+      content_tag :div do
+        content_tag(:button, current_user.name, onclick: "location.href = '../users';", class: 'btn btn-success mr-2') +
+          content_tag(:button, 'Logout', onclick: "location.href = '../logout';", class: 'btn btn-success')
+      end
+    else
+      content_tag :div do
+        content_tag(:button, 'Sign Up', onclick: "location.href = '../signup';", class: 'btn btn-success mr-2') +
+          content_tag(:button, 'Login', onclick: "location.href = '../login';", class: 'btn btn-success')
+      end
+    end
+  end
 end
