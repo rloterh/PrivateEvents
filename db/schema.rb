2d36ca853bf258ce09e6d34b001f46b947dc3790
rloterh <rloterh@gmail.com>
2020-11-22 11:41:53 +0000

    Add title field to Events model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_21_050601) do
+ActiveRecord::Schema.define(version: 2020_11_22_113917) do
 
   create_table "event_attendances", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_11_21_050601) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "creator_id"
+    t.text "title"
     t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
