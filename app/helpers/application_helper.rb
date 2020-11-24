3962f061bf9da4bd4f136536ee973e93f028f3b6
rloterh <rloterh@gmail.com>
2020-11-24 13:00:51 +0000

    Fix linter highlights

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,9 @@ module ApplicationHelper
     if logged_in?
       content_tag :div do
         content_tag(:button, 'Home', onclick: "location.href = '../';", class: 'btn btn-success mr-2') +
-        content_tag(:button, 'New Event', onclick: "location.href = '../events/new';", class: 'btn btn-success mr-2') +
-        content_tag(:button, 'Show Events', onclick: "location.href = '../show_events';", class: 'btn btn-success mr-2') +
-        content_tag(:button, current_user.name, onclick: "location.href = '../users';", class: 'btn btn-success mr-2') +
+          content_tag(:button, 'New Event', onclick: "location.href = '../events/new';", class: 'btn btn-success mr-2') +
+          content_tag(:button, 'Show Events', onclick: "location.href = '../show_events';", class: 'btn btn-success mr-2') +
+          content_tag(:button, current_user.name, onclick: "location.href = '../users';", class: 'btn btn-success mr-2') +
           content_tag(:button, 'Logout', onclick: "location.href = '../logout';", class: 'btn btn-success')
       end
     else
